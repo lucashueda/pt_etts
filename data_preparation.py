469f3f9764208c0bb34f55524902d23cfbf91830
Lucas Ueda <lucashueda@gmail.com>
2020-10-10 17:57:12 -0300

    Add trim silence

--- data_preparation.py
+++ data_preparation.py
@@ -101,7 +101,8 @@ def get_audio(audio_path, hop_length, trim_top_db = 23, n_fft = 1024):
     data_ = np.append(data_, [0.]*5*hop_length)
     data_ = data_.astype(dtype=np.int16)
     data_ = data_ / np.abs(data_).max() * 0.999
-    print(data_.min(), data_.max())
+    print(wav_max_value)
+    print(data_.min(), data_.max(), np.abs(data_).max()*0.999)
     return torch.FloatTensor(data_.astype(np.float32))
 
 def get_mel(stft, audio):
