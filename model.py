6056fd3e5745636b2c1e80189a5a3c7e98e79697
Lucas Ueda <lucashueda@gmail.com>
2020-11-02 14:15:54 -0300

    Update inference of train4

--- model.py
+++ model.py
@@ -1348,6 +1348,8 @@ class Tacotron2_EncSpeakEmb(nn.Module):
         encoder_outputs = self.encoder.inference(embedded_inputs)
 
         speaker_embedd_input = self.speaker_embedding(embedds)
+        
+        print(embedded_inputs.shape, encoder_outputs.shape, speaker_embedd_input.shape )
 
         encoder_outputs = torch.cat((encoder_outputs,speaker_embedd_input.repeat((1,encoder_outputs.size(1),1)), -1))
 
