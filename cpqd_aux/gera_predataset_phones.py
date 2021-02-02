ad0e55c870e08a4ad3d40e6780947e89e5b4ff3b
Lucas Ueda <lucashueda@gmail.com>
2021-02-02 03:12:00 -0300

    Adding phones files

--- cpqd_aux/gera_predataset_phones.py
+++ cpqd_aux/gera_predataset_phones.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
                         help='Whether to get norm text or phoneme level')
     parser.add_argument('-dn', '--df_name', type = str)
     parser.add_argument('-s', '--speaker_id', type = str)
+    parser.add_argument('-sn', '--style_name', type = str, default=None)
     args = parser.parse_args()
 
     cpqd_path = args.input_directory
@@ -70,15 +71,18 @@ if __name__ == '__main__':
                             expected_lab_file = os.path.join(lab_path, filename + '.lab')
                             if((os.path.isfile(expected_wav_file))):
                                 # print("antes do librosa")
-                                x , sr = librosa.load(expected_wav_file, sr = None)
-                                # print('antes do arquivo lab')
-                                # print(expected_lab_file, os.path.isfile(expected_lab_file))
-                                with open(expected_lab_file , 'r', encoding = 'latin-1') as f:
-                                    for k in f.readlines():
-                                        if("phones" in k[:10]):
-                                            qtde_phones = len(k[10:].replace('|', '').split())
-                                # print(len(x), sr, qtde_phones)
-                                style_targets.append(round(qtde_phones/(len(x)/sr), 3))
+                                if(args.style_name == None):
+                                    x , sr = librosa.load(expected_wav_file, sr = None)
+                                    # print('antes do arquivo lab')
+                                    # print(expected_lab_file, os.path.isfile(expected_lab_file))
+                                    with open(expected_lab_file , 'r', encoding = 'latin-1') as f:
+                                        for k in f.readlines():
+                                            if("phones" in k[:10]):
+                                                qtde_phones = len(k[10:].replace('|', '').split())
+                                    # print(len(x), sr, qtde_phones)
+                                    style_targets.append(round(qtde_phones/(len(x)/sr), 3))
+                                else:
+                                    style_targets.append(args.style_name)
                                 texts.append(line[N+2:])
                                 wav_dirs.append(expected_wav_file) 
                                 emb_ids.append(int(args.speaker_id)) # Since we dont have embedding just put that to generate correct format
@@ -87,7 +91,7 @@ if __name__ == '__main__':
                     print('deu except')
                     pass
 
-    print(len(wav_dirs), len(texts), len(emb_ids), len(style_targets), total_time/3600)
+    # print(len(wav_dirs), len(texts), len(emb_ids), len(style_targets), total_time/3600)
     df = pd.DataFrame({'wav_path':wav_dirs, 'text': texts, 'emb_id': np.array(emb_ids).astype(int), 'style_target': style_targets})
 
     df['text'] = df['text'].str.lower().str.replace('.', ' .').str.replace(',', ' ,').str.replace('!', ' !').str.replace('?', ' ?').str.replace('\n', '').str.replace('\t', '').str.replace('''"''', '')
