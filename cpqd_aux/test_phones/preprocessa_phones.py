601cc067dc535451f9ba765ed3d60cdebe534a72
Lucas Ueda <lucashueda@gmail.com>
2021-02-02 03:05:33 -0300

    Adding phones files

--- cpqd_aux/test_phones/preprocessa_phones.py
+++ cpqd_aux/test_phones/preprocessa_phones.py
@@ -13,13 +13,13 @@ map_t = {1: 't_lento',
         2: 't_medio',
         3: 't_rapido'}
 
-df_train[df_train.style_target < inter1]['style_target'] = 1
-df_train[(df_train.style_target >= inter1) & (df_train.style_target < inter2)]['style_target'] = 2
-df_train[df_train.style_target >= inter2]['style_target'] = 3
+df_train.loc[df_train.style_target < inter1, 'style_target'] = 1
+df_train.loc[(df_train.style_target >= inter1) & (df_train.style_target < inter2),'style_target'] = 2
+df_train.loc[df_train.style_target >= inter2,'style_target'] = 3
 
-df_val[df_val.style_target < inter1]['style_target'] = 1
-df_val[(df_val.style_target >= inter1) & (df_val.style_target < inter2)]['style_target'] = 2
-df_val[df_val.style_target >= inter2]['style_target'] = 3
+df_val.loc[df_val.style_target < inter1,'style_target'] = 1
+df_val.loc[(df_val.style_target >= inter1) & (df_val.style_target < inter2),'style_target'] = 2
+df_val.loc[df_val.style_target >= inter2,'style_target'] = 3
 
 df_train['style_target'] = df_train['style_target'].map(map_t)
 df_val['style_target'] = df_val['style_target'].map(map_t)
