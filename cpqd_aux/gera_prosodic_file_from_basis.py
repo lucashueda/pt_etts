e49980c2b614e762cc5ba5ffbccfb2aa1bd2c182
lucashueda <lucashueda@gmail.com>
2021-03-21 02:40:17 -0300

    Fixing gen style prodoficf files

--- cpqd_aux/gera_prosodic_file_from_basis.py
+++ cpqd_aux/gera_prosodic_file_from_basis.py
@@ -179,7 +179,8 @@ if __name__ == '__main__':
         expected_lab_file = wav[:-4] + '.lab'
 
         try:
-            if(os.path.isfile(expected_wav_file)):                    
+            if(os.path.isfile(expected_wav_file)):
+                print(expected_wav_file)                   
                 val_speaking_rate.append(get_cpqd_lab_speaking_rate(expected_wav_file, expected_lab_file))                
                 val_pitch_range.append(get_pitch_range(expected_wav_file))
                 val_energy.append(get_energy(expected_wav_file, sr = None, top_level_db=15, frame_length = 512, hop_length=128))
